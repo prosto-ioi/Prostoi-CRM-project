4b46e94bff276181a5789ded3cb93ed4d57af6ba
prostoi <prostoi@gmail.com>
2026-03-01 20:37:59 +0500

    add class UserAdmin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User 
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staf', 'is_active')
+    list_filter = ('is_active', 'is_staf', 'date_joined')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email', )
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}), 
+        ('Personal info', {'fields': ('first_name', 'last_name', 'avatar')}), 
+        ('Premissions', {'fields': ('is_staf', 'is_active', 'is_superuser', 'groups', 'user_premissions')}),
+        ('important dates', {'fields': ('last_login', 'date_joined')})
+    )
+
+    add_fieldsets = (
+        (None, {'classes': ('wide', ),
+                'fields': ('email', 'first_name', 'last_name', 'pssword1', 'password2'),
+                }),
+    )
+
+admin.site.register(User, UserAdmin)
