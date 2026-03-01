32b691797f45e69bbbe81f5b78bd40e860c8e568
prostoi <prostoi@gmail.com>
2026-03-01 21:49:15 +0500

    fiw mistakes

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -3,8 +3,8 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User 
 
 class UserAdmin(BaseUserAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'is_staf', 'is_active')
-    list_filter = ('is_active', 'is_staf', 'date_joined')
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_active', 'is_staff', 'date_joined')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email', )
 
@@ -17,7 +17,7 @@ class UserAdmin(BaseUserAdmin):
 
     add_fieldsets = (
         (None, {'classes': ('wide', ),
-                'fields': ('email', 'first_name', 'last_name', 'pssword1', 'password2'),
+                'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', ),
                 }),
     )
 
